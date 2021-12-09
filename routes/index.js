a297a3aa3e6f83bde80d9c7f5fd4af7d57bbef54
pengzer1 <81570534+pengzer1@users.noreply.github.com>
2021-12-09 17:17:49 +0900

    db pw 변경

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var client = mysql.createConnection({
   host: 'localhost',
   port: '3306',
   user: 'root',
-  password: 'ckalswo6312!',
+  password: '1234',
   database: 'neighbor',
   multipleStatements: true
 })
